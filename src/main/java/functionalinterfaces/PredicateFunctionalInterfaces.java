243708830d49afbad50475ee655d5d7212285f07
hemanthcse1 <hemanthcse1@gmail.com>
2024-06-06 12:40:59 +0530

    1. Consumer functional interface

--- src/main/java/functionalinterfaces/PredicateFunctionalInterfaces.java
+++ src/main/java/functionalinterfaces/PredicateFunctionalInterfaces.java
@@ -2,12 +2,10 @@ package functionalinterfaces;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
-public class BuiltInFunctionalInterfaces {
+public class PredicateFunctionalInterfaces {
 
     public static void main(String[] args) {
 
