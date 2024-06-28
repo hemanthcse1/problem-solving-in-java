0b781a97d731b266a22bcadd350415a1c7993911
hemanthcse1 <hemanthcse1@gmail.com>
2024-06-28 22:34:02 +0530

    Implement a Queue using Two Stacks Problem: Implement a queue using two stacks with enqueue and dequeue operations.
    Explanation: This can be achieved by using two stacks to reverse the order of elements twice.

---
+++ src/main/java/examples/interviewquestions/queues/QueueUsingStacks.java
@@ -0,0 +1,41 @@
+package examples.interviewquestions.queues;
+
+import java.util.Stack;
+
+public class QueueUsingStacks {
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public void enqueue(int item) {
+        stack1.push(item);
+    }
+
+    public int dequeue() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingStacks queue = new QueueUsingStacks();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}
