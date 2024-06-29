8d00f5c75bd8658e7b62f1be3d9993e6f733dbff
hemanthcse1 <hemanthcse1@gmail.com>
2024-06-29 20:17:43 +0530

    Container With Most Water You are given an array height of non-negative integers where each integer represents the height of a vertical line on a chart. Find two lines that together with the x-axis form a container that holds the most water.

---
+++ src/main/java/examples/interviewquestions/twopointers/ContainerWithMostWater.java
@@ -0,0 +1,29 @@
+package examples.interviewquestions.twopointers;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentArea = Math.min(height[left], height[right]) * width;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        ContainerWithMostWater solution = new ContainerWithMostWater();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = solution.maxArea(height);
+        System.out.println("Max Area: " + result);
+    }
+}
