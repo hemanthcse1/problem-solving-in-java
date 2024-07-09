a1a245ba0ba39930cf9745b5587a89816db7256a
hemanthcse1 <hemanthcse1@gmail.com>
2024-07-09 08:51:51 +0530

    remove elements

---
+++ src/main/java/examples/problems/twopointer/RemoveElement.java
@@ -0,0 +1,29 @@
+package examples.problems.twopointer;
+
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int k = removeElement(nums, val);
+
+        System.out.println("The number of elements not equal to " + val + " is: " + k);
+        System.out.print("The modified array is: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
