d0d66cdbcce01e3da2711c3ae9c71450d08ff07c
hemanthcse1 <hemanthcse1@gmail.com>
2024-07-13 10:11:32 +0530

    905. Sort Array By Parity

---
+++ src/main/java/examples/interviewquestions/twopointers/SortArrayByParity.java
@@ -0,0 +1,31 @@
+package examples.interviewquestions.twopointers;
+
+import java.util.Arrays;
+
+public class SortArrayByParity {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {3,1,2,4};
+        System.out.println("Before Parity "+ Arrays.toString(nums));
+        System.out.println("after Parity \n "+ Arrays.toString(sortArrayByParity(nums)));
+    }
+
+    public static int[] sortArrayByParity(int[] nums){
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        for (int num: nums){
+            if (num % 2 == 0){
+                result[index++] = num;
+            }
+        }
+        for (int num: nums){
+            if (num%2 != 0){
+                result[index++] = num;
+            }
+        }
+        return result;
+    }
+}
