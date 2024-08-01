7294347544bc12c6bcc348645e8aa578189f1121
hemanthcse1 <hemanthcse1@gmail.com>
2024-08-01 09:29:23 +0530

    139. Word Break

---
+++ src/main/java/examples/interviewquestions/memoization/WordBreak.java
@@ -0,0 +1,32 @@
+package examples.interviewquestions.memoization;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        WordBreak wb = new WordBreak();
+        String s = "leetcode";
+        List<String> wordDict = List.of("leet", "code");
+
+        System.out.println("Can the word be segmented? " + wb.wordBreak(s, wordDict));
+    }
+}
