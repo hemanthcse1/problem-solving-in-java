ccf3192fbbf777838be083606bc0b9a2bf093693
hemanthcse1 <hemanthcse1@gmail.com>
2024-08-05 22:19:05 +0530

    349. Intersection of Two Arrays

---
+++ src/main/java/examples/interviewquestions/arrays/IntersectionOfArrays.java
@@ -0,0 +1,36 @@
+package examples.interviewquestions.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfArrays {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] result = intersection(nums1, nums2);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> resultSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+        int[] result = new int[resultSet.size()];
+        int index = 0;
+        for (int num : resultSet) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+}
