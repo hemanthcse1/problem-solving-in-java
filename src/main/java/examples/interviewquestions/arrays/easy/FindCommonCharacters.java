855b7a7ddedd16cbd8bcd227169b636871c98705
hemanthcse1 <hemanthcse1@gmail.com>
2024-08-19 19:37:53 +0530

    1002. Find Common Characters

---
+++ src/main/java/examples/interviewquestions/arrays/easy/FindCommonCharacters.java
@@ -0,0 +1,36 @@
+package examples.interviewquestions.arrays.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindCommonCharacters {
+    public static List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+        for (String word : words) {
+            int[] charFreq = new int[26];
+            for (char c : word.toCharArray()) {
+                charFreq[c - 'a']++;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charFreq[i]);
+            }
+        }
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                result.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"bella", "label", "roller"};
+        List<String> result = commonChars(words);
+        System.out.println(result);
+    }
+}
