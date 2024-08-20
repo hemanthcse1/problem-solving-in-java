615b3899f0c23b9a32db807afc21b308e554c7db
hemanthcse1 <hemanthcse1@gmail.com>
2024-08-20 18:24:20 +0530

    1534. Count Good Triplets

---
+++ src/main/java/examples/interviewquestions/arrays/easy/CountGoodTriplets.java
@@ -0,0 +1,27 @@
+package examples.interviewquestions.arrays.easy;
+
+public class CountGoodTriplets {
+    public static int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int count = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 1, 1, 9, 7};
+        int a = 7, b = 2, c = 3;
+        System.out.println(countGoodTriplets(arr, a, b, c));
+    }
+}
