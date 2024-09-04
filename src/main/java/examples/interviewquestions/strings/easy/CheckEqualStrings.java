6bc87fcadac9f101d3592e3320d9880e9a11629b
hemanthcse1 <hemanthcse1@gmail.com>
2024-09-04 23:17:31 +0530

    2839. Check if Strings Can be Made Equal With Operations I

---
+++ src/main/java/examples/interviewquestions/strings/easy/CheckEqualStrings.java
@@ -0,0 +1,28 @@
+package examples.interviewquestions.strings.easy;
+
+import java.util.Arrays;
+
+public class CheckEqualStrings {
+
+    public boolean canBeEqual(String s1, String s2) {
+        char[] group1_s1 = {s1.charAt(0), s1.charAt(2)};
+        char[] group1_s2 = {s2.charAt(0), s2.charAt(2)};
+
+        char[] group2_s1 = {s1.charAt(1), s1.charAt(3)};
+        char[] group2_s2 = {s2.charAt(1), s2.charAt(3)};
+
+        Arrays.sort(group1_s1);
+        Arrays.sort(group1_s2);
+        Arrays.sort(group2_s1);
+        Arrays.sort(group2_s2);
+
+        return Arrays.equals(group1_s1, group1_s2) && Arrays.equals(group2_s1, group2_s2);
+    }
+
+    public static void main(String[] args) {
+        CheckEqualStrings solution = new CheckEqualStrings();
+
+        System.out.println(solution.canBeEqual("abcd", "cdab"));
+    }
+}
+
