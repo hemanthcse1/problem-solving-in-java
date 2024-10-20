30f75c13e8a1c5bd2d37afaf3117713f839f4087
hemanthcse1 <hemanthcse1@gmail.com>
2024-10-20 20:50:32 +0530

    200. Number of Islands

---
+++ src/main/java/examples/interviewquestions/arrays/medium/NumberOfIslands.java
@@ -0,0 +1,59 @@
+package examples.interviewquestions.arrays.medium;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int numberOfIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numberOfIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numberOfIslands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        NumberOfIslands solution = new NumberOfIslands();
+
+        char[][] grid1 = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+
+        char[][] grid2 = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        System.out.println("Number of islands in grid1: " + solution.numIslands(grid1));
+        System.out.println("Number of islands in grid2: " + solution.numIslands(grid2));
+    }
+}
+
