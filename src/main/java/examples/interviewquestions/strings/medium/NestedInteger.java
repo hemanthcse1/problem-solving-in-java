54a7fb715cd44edf407a9933b2525c04115db6fa
hemanthcse1 <hemanthcse1@gmail.com>
2024-10-24 15:45:10 +0530

    385. Mini Parser

---
+++ src/main/java/examples/interviewquestions/strings/medium/NestedInteger.java
@@ -0,0 +1,19 @@
+package examples.interviewquestions.strings.medium;
+
+import java.util.List;
+
+public class NestedInteger {
+    public NestedInteger() {}
+
+    public NestedInteger(int value) {}
+
+    public boolean isInteger() {}
+
+    public Integer getInteger() {}
+
+    public void setInteger(int value) {}
+
+    public void add(NestedInteger ni) {}
+
+    public List<NestedInteger> getList() {}
+}
