cbf61c22ec721a6ad474ae3673412b2b6c92af4c
hemanthcse1 <hemanthcse1@gmail.com>
2024-12-02 09:52:12 +0530

    703. Kth Largest Element in a Stream

---
+++ src/main/java/examples/interviewquestions/arrays/medium/KthLargest.java
@@ -0,0 +1,33 @@
+package examples.interviewquestions.arrays.medium;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        KthLargest kthLargest = new KthLargest(3, new int[]{4, 5, 8, 2});
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+    }
+}
