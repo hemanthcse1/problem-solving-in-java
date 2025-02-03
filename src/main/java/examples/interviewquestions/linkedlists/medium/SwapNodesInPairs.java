96367f4e2029205748123032cfd2f5b7177850e5
hemanthcse1 <hemanthcse1@gmail.com>
2025-02-03 10:12:16 +0530

    24. Swap Nodes in Pairs

---
+++ src/main/java/examples/interviewquestions/linkedlists/medium/SwapNodesInPairs.java
@@ -0,0 +1,55 @@
+package examples.interviewquestions.linkedlists.medium;
+
+public class SwapNodesInPairs {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        SwapNodesInPairs solution = new SwapNodesInPairs();
+        ListNode newHead = solution.swapPairs(head);
+
+        System.out.println("List after swapping in pairs:");
+        printList(newHead);
+    }
+}
